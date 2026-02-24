93583eba893bf5988a8ea8c3b59699296a5ec9b9
Fernando <dwk262@gmail.com>
2026-02-24 09:54:50 +0100

    Initial Commit

---
+++ Curso-ios-Apis/Curso_ios_ApisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_ApisApp.swift
+//  Curso-ios-Apis
+//
+//  Created by Equipo 9 on 24/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_ApisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
